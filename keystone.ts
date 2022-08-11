0dd29d508ba2670aff5392fe7d443e652e601fb0
a.artamonov <a.artamonov@sftpro.ru>
2022-08-11 22:34:58 +0600

    First commit

---
+++ keystone.ts
@@ -0,0 +1,12 @@
+import { config } from "@keystone-6/core"
+
+import { Post } from "./schema"
+
+export default config({
+  db: { provider: "sqlite", url: "file:./app.db" },
+  experimental: {
+    generateNextGraphqlAPI: true,
+    generateNodeAPI: true
+  },
+  lists: { Post }
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"
+import type { NextPage } from "next"
+
+import { query } from ".keystone/api"
+
+interface Post {
+  id: string
+  title: string
+  slug: string
+}
+
+interface Props {
+  posts: Array<Post>
+}
+
+const Home: NextPage<Props> = ({ posts }) => {
+  return (
+    <div>
+      <main style={{ margin: "3rem" }}>
+        <h1>Hello World! 👋🏻 </h1>
+        <ul>
+          {/* Render each post with a link to the content page */}
+          {posts.map(post => (
+            <li key={post.id}>
+              <Link href={`/post/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = await query.Post.findMany({ query: "id title slug" })
+  return { props: { posts } }
+}
+
+export default Home
